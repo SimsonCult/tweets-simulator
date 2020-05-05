ab92fd62bc004b3c39e9883c9fc06865b18a6477
SimsonCult <63821433+SimsonCult@users.noreply.github.com>
2020-05-05 03:40:14 -0300

    Create precache-manifest.84d978626c84e18b715b733862e1a717.js

---
+++ precache-manifest.84d978626c84e18b715b733862e1a717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77c1de51025cb9bcd1150dcbec86464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feac058ac605cb396f1a",
+    "url": "/static/css/main.8b97c75c.chunk.css"
+  },
+  {
+    "revision": "c21fc95143d16cc36cc7",
+    "url": "/static/js/2.80820445.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.80820445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feac058ac605cb396f1a",
+    "url": "/static/js/main.863fac5f.chunk.js"
+  },
+  {
+    "revision": "f0ef151f57df170d71a6",
+    "url": "/static/js/runtime-main.a45bf9e8.js"
+  },
+  {
+    "revision": "400dc88500c0bc5e11f6c953dedcc7ab",
+    "url": "/static/media/twitter-logo.400dc885.png"
+  }
+]);
