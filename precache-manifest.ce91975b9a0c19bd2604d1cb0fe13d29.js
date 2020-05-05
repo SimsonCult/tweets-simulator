1aacc1223e96f135cb53bde5709698a5406ee96c
SimsonCult <63821433+SimsonCult@users.noreply.github.com>
2020-05-05 02:21:26 -0300

    Add files via upload

---
+++ precache-manifest.ce91975b9a0c19bd2604d1cb0fe13d29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f73a865f86ac65a8d63021d9449435f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d87c2ca5dd73151ee1e6",
+    "url": "/static/css/main.85407e90.chunk.css"
+  },
+  {
+    "revision": "8e1520bdba4717b7e453",
+    "url": "/static/js/2.5bbef130.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5bbef130.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d87c2ca5dd73151ee1e6",
+    "url": "/static/js/main.905ce77f.chunk.js"
+  },
+  {
+    "revision": "f0ef151f57df170d71a6",
+    "url": "/static/js/runtime-main.a45bf9e8.js"
+  },
+  {
+    "revision": "400dc88500c0bc5e11f6c953dedcc7ab",
+    "url": "/static/media/twitter-logo.400dc885.png"
+  }
+]);
